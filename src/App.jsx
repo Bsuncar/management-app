6fab1dca369392aeea86388fc6082d2626a8708a
Bsuncar <118774679+Bsuncar@users.noreply.github.com>
2025-07-17 23:03:27 -0400

    Mangaing State to switch components. Also  started to collect user input with refs & forwarded refs

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,51 @@
+import { useState } from "react";
+
 import NewProject from "./components/NewProject";
 import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectsSidebar from "./components/ProjectsSidebar";
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  function handleStartAppProject() {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  }
+
+  function handleAddProject(projectData) {
+    setProjectsState((prevState) => {
+      const newPorject = {
+        ...projectData,
+        id: Math.random(),
+      };
+
+      return {
+        ...prevState,
+        projects: [...prevState.projects, newPorject],
+      };
+    });
+  }
+
+  console.log(projectsState);
+
+  let content;
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject onAdd={handleAddProject} />;
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAppProject} />;
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar />
-      <NoProjectSelected />
+      <ProjectsSidebar onStartAddProject={handleStartAppProject} />
+      {content}
     </main>
   );
 }
